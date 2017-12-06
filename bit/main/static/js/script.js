022e2c963902ef258f46cbce302ae79d8734babf
ray <ray@g.com>
2017-12-06 16:10:38 +0900

    기본

---
+++ bit/main/static/js/script.js
@@ -0,0 +1,6 @@
+/**
+ * 
+ */
+function func(){
+	alert('성공');
+}
---
+++ bit/profit/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.contrib import admin
+from profit import views
+
+
+urlpatterns = [
+    url(r'^aa', views.aa),
+
+]
